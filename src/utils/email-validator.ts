1b66925c1fdcac6d4c601736ef4405524c7a682d
levi5 <levisneto55@gmail.com>
2020-09-17 15:33:26 -0300

    feat: ensure EmailValidatorAdapter return false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,8 @@
+
+import { IEmailValidator } from '../presentation/protocols/email-validator';
+
+export class EmailValidatorAdapter implements IEmailValidator {
+	isValid (email: string): boolean | Error {
+		return false;
+	}
+}
