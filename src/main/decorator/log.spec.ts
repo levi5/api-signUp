077f7806b186d815e27400f3204efeb1c2a8a092
levi5 <levisneto55@gmail.com>
2020-10-05 12:55:38 -0300

    refactor: move sut creation to a factory method

--- src/main/decorator/log.spec.ts
+++ src/main/decorator/log.spec.ts
@@ -2,25 +2,39 @@
 import { IController, IHttpRequest, IHttpResponse } from '../../presentation/protocols';
 import { LoggerControllerDecorator } from './log';
 
+interface SutTypes {
+    sut:LoggerControllerDecorator,
+    controllerStub: IController
+}
+
+const makeController = ():IController => {
+	class ControllerStub implements IController {
+		async handle (httpRequest:IHttpRequest):Promise<IHttpResponse> {
+			const httpResponse:IHttpResponse = {
+				statusCode: 200,
+				body: {
+					name: 'test',
+					email: 'test_mail@mail.com'
+				}
+			};
+			return new Promise(resolve => resolve(httpResponse));
+		}
+	}
+	return new ControllerStub();
+};
+
+const makeSut = ():SutTypes => {
+	const controllerStub = makeController();
+	const sut = new LoggerControllerDecorator(controllerStub);
+	return {
+		sut,
+		controllerStub
+	};
+};
+
 describe('LoggerController Decorator', () => {
 	test('Should call controller handle', async () => {
-		class ControllerStub implements IController {
-			async handle (httpRequest:IHttpRequest):Promise<IHttpResponse> {
-				const httpResponse:IHttpResponse = {
-					statusCode: 200,
-					body: {
-						name: 'test',
-						email: 'test_mail@mail.com'
-					}
-				};
-
-				return new Promise(resolve => resolve(httpResponse));
-			}
-		}
-
-		const controllerStub = new ControllerStub();
-		const handleSpy = jest.spyOn(controllerStub, 'handle');
-		const sut = new LoggerControllerDecorator(controllerStub);
+		const { sut, controllerStub } = makeSut();
 		const httpRequest = {
 			body: {
 				name: 'any_name',
@@ -29,6 +43,7 @@ describe('LoggerController Decorator', () => {
 				passwordConfirmation: 'any_password'
 			}
 		};
+		const handleSpy = jest.spyOn(controllerStub, 'handle');
 		await sut.handle(httpRequest);
 		expect(handleSpy).toHaveBeenCalledWith(httpRequest);
 	});
