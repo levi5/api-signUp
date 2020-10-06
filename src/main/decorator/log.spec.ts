7e776b671a7e9a917a99f73ce7a55d2a5f73549a
levi5 <levisneto55@gmail.com>
2020-10-06 14:52:45 -0300

    test: ensure LoggerControllerDecorator returns the same result of the controller

--- src/main/decorator/log.spec.ts
+++ src/main/decorator/log.spec.ts
@@ -47,4 +47,25 @@ describe('LoggerController Decorator', () => {
 		await sut.handle(httpRequest);
 		expect(handleSpy).toHaveBeenCalledWith(httpRequest);
 	});
+
+	test('Should return the same result of the controller', async () => {
+		const { sut } = makeSut();
+		const httpRequest = {
+			body: {
+				name: 'any_name',
+				email: 'any_mail@mail.com',
+				password: 'any_password',
+				passwordConfirmation: 'any_password'
+			}
+		};
+
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse).toEqual({
+			statusCode: 200,
+			body: {
+				name: 'test',
+				email: 'test_mail@mail.com'
+			}
+		});
+	});
 });
