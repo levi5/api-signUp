6ed2999ca3ca6dcd81b74017d1ac64a3f7a62c85
levi5 <levisneto55@gmail.com>
2020-10-21 14:02:57 -0300

    feat: ensure SignUpController calls Validator with correct value

--- src/presentation/controllers/signUp/SignUp.ts
+++ src/presentation/controllers/signUp/SignUp.ts
@@ -1,18 +1,22 @@
 import { IHttpRequest, IHttpResponse, IController, IAddAccount, IEmailValidator } from './signUp-protocols';
 import { MissingParamError, InvalidParamError } from '../../errors';
 import { badRequest, serverError, success } from '../../helpers/http-helper';
+import { IValidation } from '../../helpers/validators/validation';
 
 export class SignUpController implements IController {
-	private readonly emailValidator:IEmailValidator;
 	private readonly addAccount:IAddAccount;
+	private readonly validation:IValidation;
+	private readonly emailValidator:IEmailValidator;
 
-	constructor (emailValidator:IEmailValidator, addAccount:IAddAccount) {
-		this.emailValidator = emailValidator;
+	constructor (addAccount:IAddAccount, validation:IValidation, emailValidator:IEmailValidator) {
 		this.addAccount = addAccount;
+		this.validation = validation;
+		this.emailValidator = emailValidator;
 	}
 
 	async handle (httpRequest:IHttpRequest):Promise<IHttpResponse> {
 		try {
+			this.validation.validate(httpRequest.body);
 			const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
 			for (const field of requiredFields) {
 				if (!httpRequest.body[field]) {
