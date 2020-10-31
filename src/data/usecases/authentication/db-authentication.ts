2f1b7f90d154f9e52417ddf57f27a6c7b165fe8f
levi5 <levisneto55@gmail.com>
2020-10-31 13:05:07 -0300

    feat: ensure DbAuthentication calls hashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,14 +1,21 @@
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 import { IAuthentication, IAuthenticationModel } from '../../../domain/useCases/authentication';
+import { IHashComparer } from '../../protocols/cryptography/hash-comparer';
 
 export class DbAuthentication implements IAuthentication {
-private readonly loadAccountByEmailRepository:LoadAccountByEmailRepository
-constructor (loadAccountByEmailRepository:LoadAccountByEmailRepository) {
+private readonly loadAccountByEmailRepository:LoadAccountByEmailRepository;
+private readonly hashComparer:IHashComparer;
+
+constructor (hashComparer:IHashComparer, loadAccountByEmailRepository:LoadAccountByEmailRepository) {
+	this.hashComparer = hashComparer;
 	this.loadAccountByEmailRepository = loadAccountByEmailRepository;
 }
 
 async auth (authentication: IAuthenticationModel): Promise<string> {
-	await this.loadAccountByEmailRepository.load(authentication.email);
+	const account = await this.loadAccountByEmailRepository.load(authentication.email);
+	if (account) {
+		await this.hashComparer.compare(authentication.password, account.password);
+	}
 
 	return null;
 }
--- src/domain/model/account.ts
+++ src/domain/model/account.ts
@@ -2,4 +2,5 @@ export interface IAccountModel {
     id:string,
     name:string,
     email:string,
+    password:string
 }
