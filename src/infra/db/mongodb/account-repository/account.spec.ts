2bb92488e6a3179f15bdaac3db161c6511a4fb5b
levi5 <levisneto55@gmail.com>
2020-11-15 19:26:23 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,20 @@ describe('Account Mongo Repository', () => {
 		const account = await sut.loadByEmail('any_email@email.com');
 		expect(account).toBeFalsy();
 	});
+
+	test('Should update the account accessToken on updateAccessToken success', async () => {
+		const sut = makeSut();
+		const res = await accountCollection.insertOne({
+			name: 'any_name',
+			email: 'any_email@email.com',
+			password: 'hash_password'
+		});
+		const fakeAccount = res.ops[0];
+
+		expect(fakeAccount.accessToken).toBeFalsy();
+		await sut.updateAccessToken(fakeAccount.accessToken, 'hash_password');
+		const account = await accountCollection.findOne({ _id: fakeAccount._id });
+		expect(account).toBeTruthy();
+		expect(account.password).toBe('hash_password');
+	});
 });
