58044669995c91ec48ff00203c53d6ae7b96cf8d
levi5 <levisneto55@gmail.com>
2020-12-01 12:15:04 -0300

    refactor: rename files

--- src/main/factories/useCases/add-account/db-add-account-factory.ts
+++ src/main/factories/useCases/add-account/db-add-account-factory.ts
@@ -1,5 +1,5 @@
 import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account';
-import { LoadAccountByEmailRepository } from '../../../../data/usecases/add-account/load-account-by-email';
+import { LoadAccountByEmailRepository } from '../../../../data/usecases/add-account/db-load-account-by-email';
 import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter';
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository';
 export const makeDbAddAccount = ():DbAddAccount => {
